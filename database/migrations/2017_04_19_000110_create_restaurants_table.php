f9313142a5adf8749364eea6e4bff20f55fc108f
victor <glezvic96@live.com>
2017-04-18 19:23:23 -0500

    create restaurants migration table

---
+++ database/migrations/2017_04_19_000110_create_restaurants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table( 'restaurants', function (Blueprint ) {
+            $table->increments('id');
+            $table->String('name');
+            $table->String('openAt');
+            $table->String('closeAt');
+            $table->String('ubication');
+            $table->String('slogan');
+            $table->String('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('restaurants');
+    }
+}
