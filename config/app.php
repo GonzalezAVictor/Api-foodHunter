345abf6a2f2ada9a83f08cd61a70e189e297dfee
Victor Enrique Gonzalez Arana <glezvic96@live.com>
2017-06-07 00:26:08 -0500

    .

--- config/app.php
+++ config/app.php
@@ -140,8 +140,6 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        // Jwt auth provider
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -179,6 +177,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // Jwt auth provider
+        'Tymon\JWTAuth\Providers\JWTAuthServiceProvider',
+
     ],
 
     /*
