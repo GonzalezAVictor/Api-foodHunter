575979d32cc5e444573d33a53dcc13d5371632b0
Victor Enrique Gonzalez Arana <glezvic96@live.com>
2017-06-07 23:17:06 -0500

    config ClrearDB variables

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_605488899e86b89'),
+            'username' => env('DB_USERNAME', 'b229986c0c9fea'),
+            'password' => env('DB_PASSWORD', '345ffb50'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
