63206fd30ecc2654737b1dbdf7e240f11039e114
Victor Enrique Gonzalez Arana <glezvic96@live.com>
2017-07-10 21:12:11 -0500

    set fillable NextPromotion model

--- app/NextPromotion.php
+++ app/NextPromotion.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class NextPromotion extends Model
 {
-    //
+    protected $table = 'next_promotion';
+
+    protected $fillable = [
+      'startAt',
+      'endAt',
+      'promotion_type',
+      'amount_available'
+    ];
 }
